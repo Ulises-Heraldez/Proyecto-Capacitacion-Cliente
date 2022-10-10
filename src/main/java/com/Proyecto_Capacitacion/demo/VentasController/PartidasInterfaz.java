0ccef6fa563cf2b921b3e04b963bd0a29fb9428b
Ulises-Heraldez <109759490+Ulises-Heraldez@users.noreply.github.com>
2022-10-10 10:20:21 -0600

    remove passwrod

--- src/main/java/com/Proyecto_Capacitacion/demo/VentasController/PartidasInterfaz.java
+++ src/main/java/com/Proyecto_Capacitacion/demo/VentasController/PartidasInterfaz.java
@@ -507,7 +507,7 @@ public class PartidasInterfaz extends javax.swing.JFrame {
         try {
             //UTILIZAR ENTIDADES
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/db_proyecto_capacitacion", "root", "ulises2419");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/db_proyecto_capacitacion", "root", "*****");
 
             Statement st = con.createStatement();
 
